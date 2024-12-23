aed116222c425ddacec6fb1a683242e0f48caf69
theBurn1ngMan <117646662+theBurn1ngMan@users.noreply.github.com>
2024-12-23 18:15:04 +0300

    Реализация алгоритма RLE
    В cpp-файле находятся функции для кодирования и декодирования, h-файл сделан для вызова этих функций

---
+++ RLE_Agafonov.cpp
@@ -0,0 +1,50 @@
+﻿#include "RLE_Agafonov.h"
+#include <iostream>
+#include <sstream>
+
+std::string encodeRLE(const std::string& input) {
+    if (input.empty()) return "";
+
+    std::string result;
+    char currentChar = input[0];
+    int count = 1;
+
+    for (size_t i = 1; i < input.size(); ++i) {
+        if (input[i] == currentChar) {
+            count++;
+        }
+        else {
+            result += std::to_string(count) + currentChar;
+            currentChar = input[i];
+            count = 1;
+        }
+    }
+
+    result += std::to_string(count) + currentChar;
+
+    return result;
+}
+
+std::string decodeRLE(const std::string& input) {
+    if (input.empty()) return "";
+
+    std::string result;
+    size_t index = 0;
+
+    while (index < input.size()) {
+        size_t numLength = 0;
+        while (isdigit(input[index + numLength])) {
+            numLength++;
+        }
+
+        int count = std::stoi(input.substr(index, numLength));
+
+        for (int j = 0; j < count; ++j) {
+            result.push_back(input[index + numLength]);
+        }
+
+        index += numLength + 1;
+    }
+
+    return result;
+}
---
+++ RLE_Agafonov.h
@@ -0,0 +1,9 @@
+#ifndef RLE_H
+#define RLE_H
+
+#include <string>
+
+std::string encodeRLE(const std::string& input);
+std::string decodeRLE(const std::string& input);
+
+#endif // RLE_H

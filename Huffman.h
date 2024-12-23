422c6ad39e47e2a60204d72d3623e786bd626386
gfff52 <arseniygalochkin@gmail.com>
2024-12-24 00:15:47 +0300

    Add files via upload

---
+++ Huffman.h
@@ -0,0 +1,11 @@
+#ifndef HUFFMAN_H
+#define HUFFMAN_H
+
+#include <string>
+
+namespace Huffman {
+    // Функция для выполнения кодирования и декодирования текста
+    void run(const std::string& text);
+}
+
+#endif // HUFFMAN_H
